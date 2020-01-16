7a6ff93df785541d6442cefa1767ae0454b7052a
admin <ann-kiy@mail.ru>
2020-01-16 15:17:22 +0700

    Initial commit

---
+++ src/main/java/com/kiyanitsa/animalsHotel/AnimalsHotelApplication.java
@@ -0,0 +1,13 @@
+package com.kiyanitsa.animalsHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalsHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalsHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiyanitsa/animalsHotel/AnimalsHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiyanitsa.animalsHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
