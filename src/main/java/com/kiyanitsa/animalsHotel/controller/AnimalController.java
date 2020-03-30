479a1db7e0f2006878d56675440e3192fc508cb1
admin <ann-kiy@mail.ru>
2020-03-30 21:15:28 +0700

    Дабавлен котроллер AnimalController(добавлена возможность добавления фото)

--- src/main/java/com/kiyanitsa/animalsHotel/controller/AnimalController.java
+++ src/main/java/com/kiyanitsa/animalsHotel/controller/AnimalController.java
@@ -5,7 +5,9 @@ import com.kiyanitsa.animalsHotel.services.AnimalService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -19,9 +21,10 @@ public class AnimalController {
     }
 
     @PostMapping
-    public Animal create(@RequestBody Animal animal, @AuthenticationPrincipal User user){
+    public Animal create(@RequestBody Animal animal, @RequestParam("file") MultipartFile file, @AuthenticationPrincipal User user) throws IOException {
         if(animalService.chekAnimal(animal)) {
             animal.setOwner(user);
+            animalService.addImg(animal,file);
             return animalService.getAnimalRepo().save(animal);
         }else{
             return null;
--- src/main/java/com/kiyanitsa/animalsHotel/services/AnimalService.java
+++ src/main/java/com/kiyanitsa/animalsHotel/services/AnimalService.java
@@ -6,8 +6,12 @@ import com.kiyanitsa.animalsHotel.domain.User;
 import com.kiyanitsa.animalsHotel.repo.AnimalRepo;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.UUID;
@@ -16,6 +20,8 @@ import java.util.UUID;
 @Data
 public class AnimalService {
     AnimalRepo animalRepo;
+    @Value("${upload.path}")
+    private String uploadPath;
 
     public boolean chekAnimal(Animal animal){
    //     Animal animalFromDB=animalRepo.findById(animal.getId()).orElseGet(null);
@@ -23,5 +29,17 @@ public class AnimalService {
             return false;
         }
         return true;
+    }
+    public Animal addImg(Animal animal, MultipartFile file) throws IOException {
+        if(!file.getOriginalFilename().equals("")) {
+            File uploadDir= new File(uploadPath);
+            if(!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+            String resultFileName = UUID.randomUUID().toString()+"."+file.getOriginalFilename();
+            file.transferTo(new File(uploadPath + "/" + resultFileName));
+            animal.setImg(resultFileName);
+        }
+        return animal;
     }
 }
